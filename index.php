64ac8dd6a16a8ffb894f6933534f19e1ae9a04ee
himanshup016 <himanshupise016@gmail.com>
2021-05-17 13:11:41 +0530

    header footer added

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Web Design</title>
+</head>
+<body>
+    <header>
+    <div class="container">
+    <div id="branding">
+    <h1><span style="color:rgb(230, 70, 13)">Acash</span> Web Design</h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#"><span style="color:rgb(230, 70, 13)">Home</span></a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="service.html">Services</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+    </div>
+    </header>
+    <section id="section-main">
+        <div class="container">
+        <h1>Affordable Web Design Courses</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis accusantium quae ratione laudantium nobis provident similique obcaecati veniam. Explicabo, numquam. .</p>
+        </div>
+    </section>
+    <section id="section-subscribe">
+            <div class="container">
+        <h1>Subscribe To Our Portal</h1>
+        <form >
+        <input  type="email" name="Email" placeholder="Enter Email">
+        <button class="button-1">Subscribe</button>
+        </form>
+        </div>
+    </section>
+    <section id="section-box">
+        <div class="container">
+        <div class="Box">
+            <img src="images\1.png" alt="HTML5">
+            <h2>HTML5 Markup</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, laudantium.</p>
+        </div>
+        <div class="Box">
+                <img src="images\2.png" alt="CSS3">
+                <h2>CSS3 Styling</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, laudantium.</p>
+            </div>
+        <div class="Box">
+
+                <img src="images\3.png" alt="Web Design">
+                <div class="box1"></div>
+                <h2>Web Desining</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, laudantium.</p>
+            </div>
+            </div>
+            </div>
+        </section>
+
+        <!--footer-->
+
+      <div class="footer">
+        <div class="footer-content">
+
+          <div class="footer-section about">
+            <h1 class="logo-text"><span>VRDS</span></h1>
+            <p>
+        about company.........
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 123-456-789</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@awainspires.com</span>
+        </div>
+        <div class="socials">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+          <div class="footer-section links">
+            <h2>Quick links </h2>
+            <br>
+            <ul>
+              <li><a href="about.html">About us</a></li>
+              <li><a href="service.html">Services</a></li>
+              <li><a href="contact.html">Contact</a></li>
+              <li><a href="contact.html">TermsAndConditions</a></li>
+              <li><a href="contact.html">Sight Map</a></li>
+            </ul>
+          </div>
+          <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address..."><br>
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea><br>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+        </div>
+        <div class="footer-bottom">
+        <p>Kyoto Vaccination Distribution  Ltd. &copy; 2019;</p>
+        </div>
+      </div>
+
+  </body>
+</html>
